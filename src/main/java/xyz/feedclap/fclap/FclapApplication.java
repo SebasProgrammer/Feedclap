0f0ff36bc9bcc8229b56f82ae74c9c617c548e9d
cesarmosqueira <cesarmanuel.2802@gmail.com>
2021-09-26 00:02:01 -0500

    project_init

---
+++ src/main/java/xyz/feedclap/fclap/FclapApplication.java
@@ -0,0 +1,13 @@
+package xyz.feedclap.fclap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FclapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FclapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/feedclap/fclap/FclapApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.feedclap.fclap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FclapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
