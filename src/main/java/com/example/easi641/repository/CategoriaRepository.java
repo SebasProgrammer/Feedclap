499d771e1d5d2452deeb173bc8f4d82e0398d039
sergio185678 <67492035+sergio185678@users.noreply.github.com>
2021-10-27 13:21:15 -0500

    Update CategoriaRepository.java

--- src/main/java/com/example/easi641/repository/CategoriaRepository.java
+++ src/main/java/com/example/easi641/repository/CategoriaRepository.java
@@ -2,9 +2,13 @@ package com.example.easi641.repository;
 
 import com.example.easi641.entities.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
-
+    @Query(value = "SELECT id FROM categorias c WHERE c.nombre=:nombre_categoria", nativeQuery = true)
+    Long lista_de_juego_por_categoria(String nombre_categoria);
 }
