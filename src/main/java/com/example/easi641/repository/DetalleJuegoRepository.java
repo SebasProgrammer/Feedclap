ef8486d9c53d545ba471f0969a6b7c16f6282e29
sergio185678 <67492035+sergio185678@users.noreply.github.com>
2021-10-27 13:01:12 -0500

    Update DetalleJuegoRepository.java

--- src/main/java/com/example/easi641/repository/DetalleJuegoRepository.java
+++ src/main/java/com/example/easi641/repository/DetalleJuegoRepository.java
@@ -2,10 +2,15 @@ package com.example.easi641.repository;
 
 import com.example.easi641.entities.DetalleJuego;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DetalleJuegoRepository extends JpaRepository<DetalleJuego, Long> {
-    Optional<DetalleJuego> findById(Long id);
 
+    @Query(value = "SELECT juegos_id FROM detallesjuego dj WHERE dj.categorias_id=:categoriaid", nativeQuery = true)
+    List<Long> lista_de_juego_por_categoria(Long categoriaid);
+
+    Optional<DetalleJuego> findById(Long id);
 }
